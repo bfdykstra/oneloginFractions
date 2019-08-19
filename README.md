fda673477fe4d7f25efe280fe22457f2cab6f2b6
Ben Dykstra <bfdykstra@email.wm.edu>
2019-08-19 19:13:38 -0400

    formatting, more readme instructions

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ## Instructions to run
 1. Download node.js from https://nodejs.org/en/download/
 2. Once you have node downloaded, make sure that you can execute `node -v`.
-3. From the root of the project directory in your terminal, `node calcFractions.js "your expression here"`
+3. In the terminal, navigate to the root of the project.
+4. To run the program, type `node calcFractions.js "your expression here"`
+
+
 ex: `node calcFractions.js "1/2 * 3_3/4"` will give the output `1/2 * 3_3/4 = 1_7/8`
--- calcFractions.js
+++ calcFractions.js
@@ -13,7 +13,7 @@
 
 
 /*
-Given two numbers, a and b, and a valid operator, return the result of the operation
+  Given two numbers, a and b, and a valid operator, return the result of the operation
 */
 function calcRaw(a, operator, b) {
   if (isNaN(parseFloat(a, 10)) || isNaN(parseFloat(b, 10))) throw Error(`non numeric input given: ${a}, ${b}`)
@@ -37,7 +37,9 @@ function calcRaw(a, operator, b) {
   }
 }
 
-/* Find the greatest common devisor for two numbers a and b */
+/* 
+  Find the greatest common devisor for two numbers a and b
+*/
 function greatestCommonDevisor(a, b){
   if (!b) return a;
 
@@ -45,9 +47,10 @@ function greatestCommonDevisor(a, b){
 
 }
     
-/* convert a floating point number to the string representation of a fraction
-   Doesn't do great with irrational numbers ie. 0.33333333 gives "3333333333333333/10000000000000000"
-   instead of 1/3.
+/* 
+  Convert a floating point number to the string representation of a fraction.
+  Doesn't do great with irrational numbers ie. 0.33333333 gives "3333333333333333/10000000000000000"
+  instead of 1/3.
 */
 function toFrac(float) {
   // good ole stack overflow coming in handy: 
@@ -73,6 +76,7 @@ function floatToFrac(float) {
 
   const positive = float >= 0 ? float : float * -1
   let result;
+  
   // fraction less than 1
   if (positive < 1) {
     result =  toFrac(positive);
@@ -96,7 +100,8 @@ function floatToFrac(float) {
   convert to a float
 */
 function fracToFloat(frac) {
-  const [a, b, ...rest] = frac.split('_') // either of length 2 or 1
+  const [a, b, ...rest] = frac.split('_')
+
   if (rest.length) throw Error(`Please provide a number in the valid format. The given input was: ${frac}`)
   
   let aRaw;
@@ -110,6 +115,7 @@ function fracToFloat(frac) {
 
     const [x, op, y] = b ? b.split('') : [0, '+', 0]
     bRaw = calcRaw(-1 * x, op, y)
+
   } else {
     aRaw = a.includes('/') ? calcRaw(...a.split('')) : parseFloat(a, 10)
     bRaw = b ? calcRaw(...b.split('')) : 0
@@ -139,7 +145,7 @@ function calcFractions(argArr) {
 
 function drive() {
   const expression = process.argv[2] // assumption is that input is a single string
-  const split = expression.trim().split(/\s+/); // split on whitespace, trim extra
+  const split = expression.trim().split(/\s+/); // split on whitespace, trim anything extra
 
   return calcFractions(split)
 }
